5567156085923c88b8efc9eb0f16e18df8889700
Jongyeol Yang <yangjy0113@gmail.com>
2016-10-27 20:44:02 +0900

    add parse xml jk

---
+++ script/parse_xml_jk.py
@@ -0,0 +1,35 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+from bs4 import BeautifulSoup
+import pandas as pd
+import glob
+
+data = []
+filename  = 'data/getJK_1.xml'
+file_input = open(filename)
+print "process in %s" % filename
+response_body = file_input.read()
+xml_text = BeautifulSoup(response_body, 'html.parser')
+for itemElm in xml_text.findAll('item'):
+    print itemElm
+    try:
+        data.append([itemElm.birth.string,
+                    itemElm.cntt.string,
+                    itemElm.cnty.string,
+                    itemElm.jkname.string,
+                    itemElm.ord1t.string,
+                    itemElm.ord1y.string,
+                    itemElm.ord2t.string,
+                    itemElm.ord2y.string,
+                    itemElm.part.string,
+                    itemElm.stdate.string,
+                    itemElm.wgother.string,
+                    itemElm.wgpart.string])
+    except:
+        pass
+
+print data
+data = pd.DataFrame(data)
+data.columns = ["birth", "cntT", "cntY", "jkName", "ord1T", "ord1Y", "ord2T", "ord2Y", "part", "stDate", "wgOther", "wgPart"]
+print(data)
