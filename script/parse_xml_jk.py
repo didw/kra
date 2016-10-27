d200301fd7c09260efbda7c8c0b288bd5f695b0e
Jongyeol Yang <yangjy0113@gmail.com>
2016-10-27 21:09:41 +0900

    made function

--- script/parse_xml_jk.py
+++ script/parse_xml_jk.py
@@ -5,31 +5,38 @@ from bs4 import BeautifulSoup
 import pandas as pd
 import glob
 
-data = []
-filename  = 'data/getJK_1.xml'
-file_input = open(filename)
-print "process in %s" % filename
-response_body = file_input.read()
-xml_text = BeautifulSoup(response_body, 'html.parser')
-for itemElm in xml_text.findAll('item'):
-    print itemElm
-    try:
-        data.append([itemElm.birth.string,
-                    itemElm.cntt.string,
-                    itemElm.cnty.string,
-                    itemElm.jkname.string,
-                    itemElm.ord1t.string,
-                    itemElm.ord1y.string,
-                    itemElm.ord2t.string,
-                    itemElm.ord2y.string,
-                    itemElm.part.string,
-                    itemElm.stdate.string,
-                    itemElm.wgother.string,
-                    itemElm.wgpart.string])
-    except:
-        pass
 
-print data
-data = pd.DataFrame(data)
-data.columns = ["birth", "cntT", "cntY", "jkName", "ord1T", "ord1Y", "ord2T", "ord2Y", "part", "stDate", "wgOther", "wgPart"]
-print(data)
+def parse_xml_jk(meet):
+    data = []
+    filename  = 'data/getJK_%d.xml' % meet
+    file_input = open(filename)
+    print "process in %s" % filename
+    response_body = file_input.read()
+    xml_text = BeautifulSoup(response_body, 'html.parser')
+    for itemElm in xml_text.findAll('item'):
+        print itemElm
+        try:
+            data.append([itemElm.birth.string,
+                        itemElm.cntt.string,
+                        itemElm.cnty.string,
+                        itemElm.jkname.string,
+                        itemElm.ord1t.string,
+                        itemElm.ord1y.string,
+                        itemElm.ord2t.string,
+                        itemElm.ord2y.string,
+                        itemElm.part.string,
+                        itemElm.stdate.string,
+                        itemElm.wgother.string,
+                        itemElm.wgpart.string])
+        except:
+            pass
+
+    df = pd.DataFrame(data)
+    df.columns = ["birth", "cntT", "cntY", "jkName", "ord1T", "ord1Y", "ord2T", "ord2Y", "part", "stDate", "wgOther", "wgPart"]
+    return df
+
+
+if __name__ == 'main':
+    meet = 1
+    data = parse_xml_jk(meet)
+    print data
--- script/parse_xml_tr.py
+++ script/parse_xml_tr.py
@@ -5,29 +5,35 @@ from bs4 import BeautifulSoup
 import pandas as pd
 import glob
 
-data = []
-filename  = 'data/getTR_1.xml'
-file_input = open(filename)
-print "process in %s" % filename
-response_body = file_input.read()
-xml_text = BeautifulSoup(response_body, 'html.parser')
-for itemElm in xml_text.findAll('item'):
-    print itemElm
-    try:
-        data.append([itemElm.birth.string,
-                    itemElm.cntt.string,
-                    itemElm.cnty.string,
-                    itemElm.trname.string,
-                    itemElm.ord1t.string,
-                    itemElm.ord1y.string,
-                    itemElm.ord2t.string,
-                    itemElm.ord2y.string,
-                    itemElm.part.string,
-                    itemElm.stdate.string])
-    except:
-        pass
 
-print data
-data = pd.DataFrame(data)
-data.columns = ["birth", "cntT", "cntY", "jkName", "ord1T", "ord1Y", "ord2T", "ord2Y", "part", "stDate"]
-print(data)
+def parse_xml_tr(meet):
+    data = []
+    filename  = 'data/getTR_%d.xml' % meet
+    file_input = open(filename)
+    print "process in %s" % filename
+    response_body = file_input.read()
+    xml_text = BeautifulSoup(response_body, 'html.parser')
+    for itemElm in xml_text.findAll('item'):
+        print itemElm
+        try:
+            data.append([itemElm.birth.string,
+                        itemElm.cntt.string,
+                        itemElm.cnty.string,
+                        itemElm.trname.string,
+                        itemElm.ord1t.string,
+                        itemElm.ord1y.string,
+                        itemElm.ord2t.string,
+                        itemElm.ord2y.string,
+                        itemElm.part.string,
+                        itemElm.stdate.string])
+        except:
+            pass
+
+    df = pd.DataFrame(data)
+    df.columns = ["birth", "cntT", "cntY", "jkName", "ord1T", "ord1Y", "ord2T", "ord2Y", "part", "stDate"]
+    return df
+
+if __name__ == 'main':
+    meet = 1
+    data = parse_xml_tr(meet)
+    print(data)
