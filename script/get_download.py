68015bd5e4e9b71705021711a1e5c2e82c7ebd1d
Jongyeol Yang <yangjy0113@gmail.com>
2016-10-27 20:44:51 +0900

    download path change

--- script/get_download.py
+++ script/get_download.py
@@ -15,19 +15,19 @@ import time
 # http://race.kra.co.kr/dbdata/fileDownLoad.do?fn=internet/seoul/jockey/20161023sdb2.txt&meet=1
 # 조교사정보. 목일 업데이트
 # http://race.kra.co.kr/dbdata/fileDownLoad.do?fn=internet/seoul/trainer/20161023sdb3.txt&meet=1
-race_url = "http://race.kra.co.kr/dbdata/fileDownLoad.do?fn=chollian/seoul/girok/horse-rslt/"
+race_url = "http://race.kra.co.kr/dbdata/fileDownLoad.do?fn=internet/seoul/horse/"
 meet = 1  # 1: seoul, 2: jeju, 3: bukyeong
 date = datetime.date.today() + datetime.timedelta(days=1)
-date = datetime.date(2015, 5, 25)
+#date = datetime.date(2015, 5, 25)
 while date > datetime.date(2011, 01, 01):
     date += datetime.timedelta(days=-1)
-    if date.weekday() not in [6]:
+    if date.weekday() not in [3, 6]:
         continue
     date_s = int("%d%02d%02d" % (date.year, date.month, date.day))
-    request = "%s%ddacom21.rpt&meet=%d" % (race_url, date_s, meet)
+    request = "%s%dsdb1.txt&meet=%d" % (race_url, date_s, meet)
     try:
         response_body = urlopen(request).read()
-        fout = open("download/horse-rslt/horse-rslt_%d_%d.txt" % (meet, date_s), 'w')
+        fout = open("../download/horse/horse_%d_%d.txt" % (meet, date_s), 'w')
         fout.write(response_body)
         fout.close()
         print "[%s] data is downloaded" % date_s
