f2c625b0b1f350babc1b1cc21d89c7efeaf3813a
Jongyeol Yang <yangjy0113@gmail.com>
2016-10-30 21:58:07 +0900

    let's predict

--- script/predict_next.py
+++ script/predict_next.py
@@ -5,26 +5,61 @@ from bs4 import BeautifulSoup
 import pandas as pd
 import glob
 import parse_xml_entry as xe
-import parse_xml_jk as xj
-import parse_xml_race as xr
-import parse_xml_tr as xt
+import datetime
+import train as tr
 
-def predict_next():
-    meet = 1
-    date = "201610"
+def normalize_data(org_data):
+    data = org_data.dropna()
+    data = data.reset_index()
+    data['gender'][data['gender'] == '암'] = 1
+    data['gender'][data['gender'] == '수'] = 1
+    data['gender'][data['gender'] == '거'] = 2
+    data['cntry'][data['cntry'] == '한'] = 0
+    data['cntry'][data['cntry'] == '한(포)'] = 1
+    data['cntry'][data['cntry'] == '미'] = 2
+    data['cntry'][data['cntry'] == '뉴'] = 3
+    data['cntry'][data['cntry'] == '호'] = 3
+    data['cntry'][data['cntry'] == '일'] = 4
+    data['cntry'][data['cntry'] == '캐'] = 5
+    data['cntry'][data['cntry'] == '브'] = 6
+    data['cntry'][data['cntry'] == '남'] = 6
+    data['cntry'][data['cntry'] == '아일'] = 7
+    data['cntry'][data['cntry'] == '모'] = 8
+    return data
+
+
+def predict_next(bd, ed, meet, date):
+    estimator = tr.training(bd, ed)
     data = xe.parse_xml_entry(meet, date)
-    print(data)
-    df = pd.DataFrame()
-    df.columns = ['course', 'humidity', 'rank', 'idx', 'name', 'cntry', 'gender', 'age', 'budam', 'jockey', \
-                  'trainer', 'owner', 'weight', 'dweight', 'rctime', 'r1', 'r2', 'r3', 'hr_days', 'hr_t1', \
-                  'hr_t2', 'hr_y1', 'hr_y2', 'jk_t1', 'jk_t2', 'jk_y1', 'jk_y2', 'tr_t1', 'tr_t2', 'tr_y1', \
-                  'tr_y2']
+    data = normalize_data(data)
+    X_data = data.copy()
+    del X_data['name']
+    del X_data['jockey']
+    del X_data['trainer']
+    del X_data['owner']
+    pred = estimator.predict(X_data)
+    prev_rc = data['rcno'][0]
+    for idx, row in data.iterrows():
+        rctime = []
+        rcdata = []
+        if row['rcno'] == prev_rc:
+            rctime.append(pred[idx])
+            rcdata.append([row['rcno'], row['name'], data['idx']])
+        else:
+            rctime = pd.Series(rctime)
+            rcdata = pd.DataFrame(rcdata)
+            rcrank = rctime.rank()
+            for i, v in enumerate(rcrank):
+                if v == 1:
+                    print("rcNo: %s, 1st: %d (%s)" % (rcdata['rcno'][i], rcdata['idx'][i], rcdata['name'][i]))
+                elif v == 2:
+                    print("rcNo: %s, 2nd: %d (%s)" % (rcdata['rcno'][i], rcdata['idx'][i], rcdata['name'][i]))
 
 
 if __name__ == '__main__':
     meet = 1
     date = 201610
-    data = xe.parse_xml_entry(meet, date)
-    print data
+    predict_next(datetime.date(2016, 9, 1), datetime.date(2016, 9, 30), meet, date)
+    predict_next(meet, date)
 
 
