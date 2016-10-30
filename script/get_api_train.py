505639d0e94ad67c75b0b04445abf89c97aa1818
Jongyeol Yang <yangjy0113@gmail.com>
2016-10-30 19:48:05 +0900

    add to get train data

---
+++ script/get_api_train.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+from urllib2 import Request, urlopen
+from urllib import urlencode, quote_plus
+race_url = "http://data.kra.co.kr/publicdata/service/train/getTrain"
+service_key = "MZBYd4tuPPcTF%2Flqt01Rco4IPTC3r5SZDRbDnoW5P7XG3aCIMMGepC0D%2FnKo1Yu5OVyDYjcAk9l3qg34t6XGzA%3D%3D"
+meet = 1  # 1: seoul, 2: jeju, 3: bukyeong
+date = 201610
+
+url = "%s?meet=%d&trainData=%s&ServiceKey=%s" % (race_url, meet, date, service_key)
+print(url)
+request = Request(url)
+request.get_method = lambda: 'GET'
+response_body = urlopen(request).read()
+fout = open("../xml/getTrain_%d_%d.xml" % (date, meet), 'w')
+fout.write(response_body)
+fout.close()
+print "download completed"
