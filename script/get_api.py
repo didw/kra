67fc92d4223c2f9dc200317620d878f2b40eb878
Jongyeol Yang <yangjy0113@gmail.com>
2016-11-01 21:32:02 +0900

    get data from api

---
+++ script/get_api.py
@@ -0,0 +1,13 @@
+import get_api_entry as ae
+import get_api_hr as ah
+import get_api_jk as aj
+import get_api_tr as at
+import get_api_train as atrain
+
+
+ae.get_data(1, 201611)
+ah.get_data(1)
+aj.get_data(1)
+at.get_data(1)
+atrain.get_data(1, 201610)
+
