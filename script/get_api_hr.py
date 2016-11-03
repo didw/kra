210bb6fafeb91675b5e134c1165d175aa3273986
jyyang <yangjy0113@gmail.com>
2016-11-03 22:10:46 +0900

    minor fix

--- script/get_api_hr.py
+++ script/get_api_hr.py
@@ -2,7 +2,7 @@
 # -*- coding: utf-8 -*-
 
 from urllib2 import Request, urlopen
-from urllib import urlencode, quote_plus
+
 def get_data(meet):
     race_url = "http://data.kra.co.kr/publicdata/service/hr/getHR"
     service_key = "MZBYd4tuPPcTF%2Flqt01Rco4IPTC3r5SZDRbDnoW5P7XG3aCIMMGepC0D%2FnKo1Yu5OVyDYjcAk9l3qg34t6XGzA%3D%3D"
