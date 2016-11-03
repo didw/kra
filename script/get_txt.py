56df62ca1e3ac66c00775c7edc692a29ed76e0e7
jyyang <yangjy0113@gmail.com>
2016-11-03 22:10:35 +0900

    make function download_txt

--- script/get_txt.py
+++ script/get_txt.py
@@ -5,32 +5,43 @@ from urllib2 import Request, urlopen
 import datetime
 import time
 
-# 경마성적표. 토일 업데이트
-# http://race.kra.co.kr/dbdata/fileDownLoad.do?fn=chollian/seoul/jungbo/rcresult/20161023dacom11.rpt&meet=1
-# 말성적조회. 일 업데이트
-# http://race.kra.co.kr/dbdata/fileDownLoad.do?fn=chollian/seoul/girok/horse-rslt/20161023dacom21.rpt&meet=1
-# 경주마정보. 목일 업데이트
-# http://race.kra.co.kr/dbdata/fileDownLoad.do?fn=internet/seoul/horse/20161023sdb1.txt&meet=1
-# 기수정보. 목일 업데이트
-# http://race.kra.co.kr/dbdata/fileDownLoad.do?fn=internet/seoul/jockey/20161023sdb2.txt&meet=1
-# 조교사정보. 목일 업데이트
-# http://race.kra.co.kr/dbdata/fileDownLoad.do?fn=internet/seoul/trainer/20161023sdb3.txt&meet=1
-race_url = "http://race.kra.co.kr/dbdata/fileDownLoad.do?fn=chollian/seoul/jungbo/rcresult/"
-meet = 1  # 1: seoul, 2: jeju, 3: bukyeong
-date = datetime.date.today() + datetime.timedelta(days=1)
-while date > datetime.date(2016, 10, 01):
-    date += datetime.timedelta(days=-1)
-    if date.weekday() not in [5, 6]:
-        continue
-    date_s = int("%d%02d%02d" % (date.year, date.month, date.day))
-    request = "%s%ddacom11.rpt&meet=%d" % (race_url, date_s, meet)
-    try:
-        response_body = urlopen(request).read()
-        fout = open("../txt/rcresult/rcresult_%d_%d.txt" % (meet, date_s), 'w')
-        fout.write(response_body)
-        fout.close()
-        print "[%s] data is downloaded" % request
-    except:
-        print '[%s] data downloading failed' % request
+# http://race.kra.co.kr/dbdata/fileDownLoad.do?fn=chollian/busan/jungbo/rcresult/20161030dacom11.rpt&meet=3
+def download_txt(bd, ed, meet):
+    data = [# seoul
+            [["http://race.kra.co.kr/dbdata/fileDownLoad.do?fn=chollian/seoul/jungbo/rcresult/", "rcresult", "dacom11.rpt", [5, 6]],
+             ["http://race.kra.co.kr/dbdata/fileDownLoad.do?fn=internet/seoul/horse/", "horse", "sdb1.txt", [3, 6]],
+             ["http://race.kra.co.kr/dbdata/fileDownLoad.do?fn=internet/seoul/jockey/", "jockey", "sdb2.txt", [3, 6]],
+             ["http://race.kra.co.kr/dbdata/fileDownLoad.do?fn=internet/seoul/trainer/", "trainer", "sdb3.txt", [3, 6]]],
+            # jeju
+            [["http://race.kra.co.kr/dbdata/fileDownLoad.do?fn=chollian/jeju/jungbo/rcresult/", "rcresult", "dacom11.rpt", [4, 5]],
+             ["http://race.kra.co.kr/dbdata/fileDownLoad.do?fn=internet/jeju/rcresult/", "horse", "cdb1.txt", [2, 5]],
+             ["http://race.kra.co.kr/dbdata/fileDownLoad.do?fn=internet/jeju/rcresult/", "jockey", "cdb2.txt", [2, 5]],
+             ["http://race.kra.co.kr/dbdata/fileDownLoad.do?fn=internet/jeju/rcresult/", "trainer", "cdb3.txt", [2, 5]]],
+            # busan
+            [["http://race.kra.co.kr/dbdata/fileDownLoad.do?fn=chollian/busan/jungbo/rcresult/", "rcresult", "dacom11.rpt", [4, 6]],
+             ["http://race.kra.co.kr/dbdata/fileDownLoad.do?fn=internet/busan/rcresult/", "horse", "pdb1.txt", [2, 6]],
+             ["http://race.kra.co.kr/dbdata/fileDownLoad.do?fn=internet/busan/rcresult/", "jockey", "pdb2.txt", [2, 6]],
+             ["http://race.kra.co.kr/dbdata/fileDownLoad.do?fn=internet/busan/rcresult/", "trainer", "pdb3.txt", [2, 6]]]
+            ]
 
-print "job has completed"
+    for line in data[meet-1]:
+        race_url = line[0]
+        date = bd + datetime.timedelta(days=-1)
+        while date < ed:
+            date += datetime.timedelta(days=1)
+            if date.weekday() not in line[3]:
+                continue
+            date_s = int("%d%02d%02d" % (date.year, date.month, date.day))
+            request = "%s%d%s&meet=%d" % (race_url, date_s, line[2], meet)
+            try:
+                response_body = urlopen(request).read()
+                fout = open("../txt/%d/%s/%s_%d_%d.txt" % (meet, line[1], line[1], meet, date_s), 'w')
+                fout.write(response_body)
+                fout.close()
+                print "[%s] data is downloaded" % request
+            except:
+                print '[%s] data downloading failed' % request
+    print "job has completed"
+
+if __name__ == '__main__':
+    download_txt(datetime.date(2016, 10, 1), datetime.date.today(), 1)
