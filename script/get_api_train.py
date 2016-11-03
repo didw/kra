ee2f7072c0b7e7d0d1e07ff9972869087017920f
jyyang <yangjy0113@gmail.com>
2016-11-03 18:11:04 +0900

    minor bug fix

--- script/get_api_train.py
+++ script/get_api_train.py
@@ -7,8 +7,6 @@ from urllib import urlencode, quote_plus
 def get_data(meet, date):
     race_url = "http://data.kra.co.kr/publicdata/service/train/getTrain"
     service_key = "MZBYd4tuPPcTF%2Flqt01Rco4IPTC3r5SZDRbDnoW5P7XG3aCIMMGepC0D%2FnKo1Yu5OVyDYjcAk9l3qg34t6XGzA%3D%3D"
-    meet = 1  # 1: seoul, 2: jeju, 3: bukyeong
-    date = 201610
 
     url = "%s?meet=%d&trainData=%s&ServiceKey=%s" % (race_url, meet, date, service_key)
     request = Request(url)
