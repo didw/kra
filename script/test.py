9a21f719f7f20f59d89446cb77330898a9678699
jyyang <yangjy0113@gmail.com>
2016-11-05 21:41:06 +0900

    test get distance_record function

--- script/test.py
+++ script/test.py
@@ -64,29 +64,40 @@ def test_random():
     print(random.randint(1, total))
 
 
-def get_distance_record(hrname, date):
-    url = "http://race.kra.co.kr/chulmainfo/chulmaDetailInfoDistanceRecord.do?Act=02&Sub=1&meet=1&rcNo=5&rcDate=%d" % date
+def get_distance_record(hrname, rcno, date):
+    print("name: %s, rcno: %d, date: %d" % (hrname, rcno, date))
+    url = "http://race.kra.co.kr/chulmainfo/chulmaDetailInfoDistanceRecord.do?Act=02&Sub=1&meet=1&rcNo=%d&rcDate=%d" % (rcno, date)
     response_body = urlopen(url).read()
     line = unicode(response_body, 'euc-kr').encode('utf-8')
-    print("%s" % line)
-    #exp = '%s[\w\s]+<td>\d+[.]\d+</td>\s+<td>\d+[.]\d+</td>\s+<td>\d+[:]\d+[.]\d+\w</td>\s+<td>\d+[:]\d+[.]\d+\w</td>\s+<td>\d+[:]\d+[.]\d+\w</td>' % hrname
+    print(line)
     exp = '%s.+\s+.+\s+<td>\d+[.]\d+</td>\s+<td>\d+[.]\d+</td>\s+<td>\d+[:]\d+[.]\d+.+</td>\s+<td>\d+[:]\d+[.]\d+.+</td>\s+<td>\d+[:]\d+[.]\d+.+</td>' % hrname
     p = re.compile(unicode(r'%s' % exp, 'utf-8').encode('utf-8'), re.MULTILINE)
     pl = p.search(line)
-    res = 10
+    res = [-1, -1, -1, -1, -1, -1]
     if pl is not None:
         pls = pl.group().split()
-        res[0] = re.compile(unicode(r'\d+(?=\()', 'utf-8').encode('utf-8'), pls[2]).group()
-        res[1] = re.compile(unicode(r'\d+[.]\d+', 'utf-8').encode('utf-8'), pls[3]).group()
-        res[2] = re.compile(unicode(r'\d+[.]\d+', 'utf-8').encode('utf-8'), pls[4]).group()
-        res[3] = re.compile(unicode(r'\d+[:]\d+[.]\d+', 'utf-8').encode('utf-8'), pls[5]).group()
-        res[4] = re.compile(unicode(r'\d+[:]\d+[.]\d+', 'utf-8').encode('utf-8'), pls[6]).group()
-        res[5] = re.compile(unicode(r'\d+[:]\d+[.]\d+', 'utf-8').encode('utf-8'), pls[7]).group()
+        res[0] = re.search(unicode(r'\d+(?=\()', 'utf-8').encode('utf-8'), pls[2]).group()
+        res[1] = re.search(unicode(r'\d+[.]\d+', 'utf-8').encode('utf-8'), pls[3]).group()
+        res[2] = re.search(unicode(r'\d+[.]\d+', 'utf-8').encode('utf-8'), pls[4]).group()
+        t = re.search(unicode(r'\d+[:]\d+[.]\d+', 'utf-8').encode('utf-8'), pls[5]).group()
+        res[3] = int(t.split(':')[0])*600 + int(t.split(':')[1].split('.')[0])*10 + int(t.split('.')[1])
+        t = re.search(unicode(r'\d+[:]\d+[.]\d+', 'utf-8').encode('utf-8'), pls[6]).group()
+        res[4] = int(t.split(':')[0])*600 + int(t.split(':')[1].split('.')[0])*10 + int(t.split('.')[1])
+        t = re.search(unicode(r'\d+[:]\d+[.]\d+', 'utf-8').encode('utf-8'), pls[7]).group()
+        res[5] = int(t.split(':')[0])*600 + int(t.split(':')[1].split('.')[0])*10 + int(t.split('.')[1])
     else:
-        print("None")
+        print("can not find %s in %s" % (hrname, url))
     return res
+
+def get_grade():
+    str = "(서울) 제82일 1300M 국6    별정A       경주명 : 일반 "
+    kind = re.search(unicode(r'M.+\d', 'utf-8').encode('utf-8'), str)
+    if kind is None:
+        print("none")
+    else:
+        print(kind.group()[-1])
 
 
 if __name__ == '__main__':
-    print(get_distance_record("야호챔피언", 20161105))
+    print(get_distance_record('캡틴로드', 3, 20140831))
 
