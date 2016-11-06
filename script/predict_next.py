f4f166e22f800d5a03a52065e4253b0e41af7815
jyyang <yangjy0113@gmail.com>
2016-11-06 20:01:36 +0900

    update predict module

--- script/predict_next.py
+++ script/predict_next.py
@@ -18,22 +18,29 @@ def normalize_data(org_data):
     data['cntry'][data['cntry'] == '한(포)'] = 1
     data['cntry'][data['cntry'] == '미'] = 2
     data['cntry'][data['cntry'] == '뉴'] = 3
-    data['cntry'][data['cntry'] == '호'] = 3
-    data['cntry'][data['cntry'] == '일'] = 4
-    data['cntry'][data['cntry'] == '캐'] = 5
-    data['cntry'][data['cntry'] == '브'] = 6
-    data['cntry'][data['cntry'] == '남'] = 6
-    data['cntry'][data['cntry'] == '아일'] = 7
-    data['cntry'][data['cntry'] == '모'] = 8
+    data['cntry'][data['cntry'] == '호'] = 4
+    data['cntry'][data['cntry'] == '일'] = 5
+    data['cntry'][data['cntry'] == '캐'] = 6
+    data['cntry'][data['cntry'] == '브'] = 7
+    data['cntry'][data['cntry'] == '헨'] = 8
+    data['cntry'][data['cntry'] == '남'] = 9
+    data['cntry'][data['cntry'] == '아일'] = 10
+    data['cntry'][data['cntry'] == '모'] = 11
+    data['cntry'][data['cntry'] == '영'] = 12
+    data['cntry'][data['cntry'] == '인'] = 13
+    data['cntry'][data['cntry'] == '아'] = 14
+    data['cntry'][data['cntry'] == '중'] = 15
+    data['cntry'][data['cntry'] == '프'] = 16
     return data
 
 
-"""['course', 'humidity', 'idx', 'name', 'cntry', 'gender', 'age', 'budam', 'jockey', 'trainer', 'owner',
- 'weight', 'dweight', 'hr_days', 'hr_nt', 'hr_nt1', 'hr_nt2', 'hr_t1', 'hr_t2', 'hr_ny', 'hr_ny1',
- 'hr_ny2', 'hr_y1', 'hr_y2', 'jk_nt', 'jk_nt1', 'jk_nt2', 'jk_t1', 'jk_t2', 'jk_ny', 'jk_ny1',
- 'jk_ny2', 'jk_y1', 'jk_y2', 'tr_nt', 'tr_nt1', 'tr_nt2', 'tr_t1', 'tr_t2', 'tr_ny', 'tr_ny1',
- 'tr_ny2', 'tr_y1', 'tr_y2', 'rcno']
- """
+"""['course', 'humidity', 'kind', 'rank', 'idx', 'name', 'cntry', 'gender', 'age', 'budam', 'jockey', 'trainer', # 12
+  'owner', 'weight', 'dweight', 'rctime', 'r1', 'r2', 'r3', 'cnt', 'rcno', 'month', # 10
+  'hr_days', 'hr_nt', 'hr_nt1', 'hr_nt2', 'hr_t1', 'hr_t2', 'hr_ny', 'hr_ny1', 'hr_ny2', 'hr_y1', 'hr_y2', # 11
+  'hr_dt', 'hr_d1', 'hr_d2', 'hr_rh', 'hr_rm', 'hr_rl', # 6
+  'jk_nt', 'jk_nt1', 'jk_nt2', 'jk_t1', 'jk_t2', 'jk_ny', 'jk_ny1', 'jk_ny2', 'jk_y1', 'jk_y2', # 10
+  'tr_nt', 'tr_nt1', 'tr_nt2', 'tr_t1', 'tr_t2', 'tr_ny', 'tr_ny1', 'tr_ny2', 'tr_y1', 'tr_y2'] # 10
+  """
 def print_log(data, pred, fname):
     flog = open(fname, 'w')
     rcno = 1
@@ -54,19 +61,18 @@ def print_log(data, pred, fname):
 
 
 def predict_next(estimator, meet, date):
-    data = xe.parse_xml_entry(meet, date)
-    data = normalize_data(data)
+    data_pre = xe.parse_xml_entry(meet, date)
+    data = normalize_data(data_pre)
     X_data = data.copy()
     del X_data['name']
     del X_data['jockey']
     del X_data['trainer']
     del X_data['owner']
-    del X_data['rcno']
     pred = pd.DataFrame(estimator.predict(X_data))
     pred.columns = ['predict']
     __DEBUG__ = True
     if __DEBUG__:
-        print_log(data, pred, '../log/%s.txt' % date)
+        pd.concat([data_pre, pred], axis=1).to_csv('../log/%d_xml.csv' % date, index=False)
     print(pd.concat([data, pred], axis=1))
     print(pd.concat([data[['rcno', 'name', 'jockey', 'trainer']], pred], axis=1))
     prev_rc = data['rcno'][0]
@@ -96,10 +102,10 @@ def predict_next(estimator, meet, date):
 
 if __name__ == '__main__':
     meet = 1
-    date = 20161105
+    date = 20161106
     #import get_api
     #get_api.get_data(meet, date/100)
-    estimator = tr.training(datetime.date(2011, 2, 1), datetime.date(2016, 10, 31))
+    estimator = tr.training(datetime.date(2011, 1, 1), datetime.date(2016, 10, 31))
     predict_next(estimator, meet, date)
 
 
