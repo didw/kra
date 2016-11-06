59ce6270505d36a1a238caa1f1233045718ebb43
jyyang <yangjy0113@gmail.com>
2016-11-06 20:00:52 +0900

    increase 10 more columns

--- script/parse_xml_entry.py
+++ script/parse_xml_entry.py
@@ -119,6 +119,71 @@ def get_tr_win(data, name):
     return res
 
 
+def get_distance_record_url(hrname, rcno, date):
+    #print("name: %s, rcno: %d, date: %d" % (hrname, rcno, date))
+    url = "http://race.kra.co.kr/chulmainfo/chulmaDetailInfoDistanceRecord.do?Act=02&Sub=1&meet=1&rcNo=%d&rcDate=%d" % (rcno, date)
+    response_body = urlopen(url).read()
+    line = unicode(response_body, 'euc-kr').encode('utf-8')
+    #print("%s" % line)
+    exp = '%s.+\s+.+\s+<td>\d+[.]\d+</td>\s+<td>\d+[.]\d+</td>\s+<td>\d+[:]\d+[.]\d+.+</td>\s+<td>\d+[:]\d+[.]\d+.+</td>\s+<td>\d+[:]\d+[.]\d+.+</td>' % hrname.encode("utf-8")
+    #print("exp: %s" % exp)
+    pl = re.search(r'%s'%exp, line)
+    res = [-1, -1, -1, -1, -1, -1]
+    if pl is not None:
+        pls = pl.group().split()
+        res[0] = re.search(unicode(r'\d+(?=\()', 'utf-8').encode('utf-8'), pls[2]).group()
+        res[1] = re.search(unicode(r'\d+[.]\d+', 'utf-8').encode('utf-8'), pls[3]).group()
+        res[2] = re.search(unicode(r'\d+[.]\d+', 'utf-8').encode('utf-8'), pls[4]).group()
+        t = re.search(unicode(r'\d+[:]\d+[.]\d+', 'utf-8').encode('utf-8'), pls[5]).group()
+        res[3] = int(t.split(':')[0])*600 + int(t.split(':')[1].split('.')[0])*10 + int(t.split('.')[1])
+        t = re.search(unicode(r'\d+[:]\d+[.]\d+', 'utf-8').encode('utf-8'), pls[6]).group()
+        res[4] = int(t.split(':')[0])*600 + int(t.split(':')[1].split('.')[0])*10 + int(t.split('.')[1])
+        t = re.search(unicode(r'\d+[:]\d+[.]\d+', 'utf-8').encode('utf-8'), pls[7]).group()
+        res[5] = int(t.split(':')[0])*600 + int(t.split(':')[1].split('.')[0])*10 + int(t.split('.')[1])
+    else:
+        print("can not find %s in %s" % (hrname, url))
+    return res
+
+def get_game_info(date, rcno):
+    if date.weekday() == 5:
+        file_date = date + datetime.timedelta(days=-2)
+    if date.weekday() == 6:
+        file_date = date + datetime.timedelta(days=-3)
+    fname = '../txt/1/chulma/chulma_1_%d%02d%02d.txt' % (file_date.year, file_date.month, file_date.day)
+    #print(fname)
+    finput = open(fname)
+    date_s = "%d[.]%02d[.]%02d" % (date.year % 100, date.month, date.day)
+    exp = "%s.*%d" % (date_s, rcno)
+    #print("%s" % exp)
+    found = False
+    for _ in range(3000):
+        line = finput.readline()
+        if not line:
+            break
+        line = unicode(line, 'euc-kr').encode('utf-8')
+        if re.search(unicode(r'%s' % exp, 'utf-8').encode('utf-8'), line) is not None:
+            found = True
+            break
+    if not found:
+        return [-1, -1]
+    for _ in range(5):
+        line = finput.readline()
+        if not line:
+            break
+        line = unicode(line, 'euc-kr').encode('utf-8')
+        print("%s" % line)
+        num = re.search(unicode(r'(?<=출전:)[\s\d]+(?=두)', 'utf-8').encode('utf-8'), line)
+        kind = re.search(unicode(r'\d+(?=등급)', 'utf-8').encode('utf-8'), line)
+        if num is not None:
+            if kind is None:
+                kind = 0
+            else:
+                kind = kind.group()[-1]
+            return [num.group(), kind]
+    return [-1, -1]
+
+
+
 def parse_xml_entry(meet, date):
     # get other data
     data_hr = xh.parse_xml_hr(meet)
@@ -134,22 +199,20 @@ def parse_xml_entry(meet, date):
     humidity = get_humidity()
     for itemElm in xml_text.findAll('item'):
         #print itemElm
-        """['course', 'humidity', 'rank', 'idx', 'name', 'cntry', 'gender', 'age', 'budam', 'jockey', \
-          'trainer', 'owner', 'weight', 'dweight', 'rctime', 'r1', 'r2', 'r3', 'hr_days', 'hr_t1', \
-          'hr_t2', 'hr_y1', 'hr_y2', 'jk_t1', 'jk_t2', 'jk_y1', 'jk_y2', 'tr_t1', 'tr_t2', 'tr_y1', \
-          'tr_y2']
-        """
         rcdate = int("%s%s%s" % (itemElm.rcdate.string[:4], itemElm.rcdate.string[5:7], itemElm.rcdate.string[8:10]))
         if date != rcdate:
             continue
         hr_gender, hr_days = get_hr_data(data_hr, itemElm.hrname.string)
         hr_weight, hr_dweight = get_hr_weight(meet, itemElm.rcdate.string, itemElm.rcno.string, itemElm.hrname.string)
+        hr_dist_rec = get_distance_record_url(itemElm.hrname.string, int(itemElm.rcno.string), date)
+        cnt, kind = get_game_info(datetime.date(date/10000, date/100%100, date%100), int(itemElm.rcno.string))
         hr_win = get_hr_win(itemElm.cntt.string, itemElm.ord1t.string, itemElm.ord2t.string, itemElm.cnty.string,
                            itemElm.ord1y.string, itemElm.ord2y.string)
         jk_win = get_jk_win(data_jk, itemElm.jkname.string)
         tr_win = get_tr_win(data_tr, itemElm.trname.string)
         adata = [itemElm.rcdist.string,
                  humidity,
+                 kind,
                  itemElm.chulno.string,
                  itemElm.hrname.string,
                  itemElm.prdctyname.string,
@@ -162,6 +225,9 @@ def parse_xml_entry(meet, date):
                  itemElm.owname.string,
                  hr_weight,
                  hr_dweight,
+                 cnt,
+                 itemElm.rcno.string,
+                 date/100%100,
                  hr_days,
 
                  itemElm.cntt.string,
@@ -174,6 +240,13 @@ def parse_xml_entry(meet, date):
                  itemElm.ord2y.string,
                  hr_win[2],
                  hr_win[3],
+
+                 hr_dist_rec[0],
+                 hr_dist_rec[1],
+                 hr_dist_rec[2],
+                 hr_dist_rec[3],
+                 hr_dist_rec[4],
+                 hr_dist_rec[5],
 
                  jk_win[0],
                  jk_win[1],
@@ -196,23 +269,22 @@ def parse_xml_entry(meet, date):
                  tr_win[7],
                  tr_win[8],
                  tr_win[9],
-
-                itemElm.rcno.string
                 ]
         #print(adata)
         data.append(adata)
 
     df = pd.DataFrame(data)
-    df.columns = ['course', 'humidity', 'idx', 'name', 'cntry', 'gender', 'age', 'budam', 'jockey', 'trainer', 'owner',
-                  'weight', 'dweight', 'hr_days', 'hr_nt', 'hr_nt1', 'hr_nt2', 'hr_t1', 'hr_t2', 'hr_ny', 'hr_ny1',
-                  'hr_ny2', 'hr_y1', 'hr_y2', 'jk_nt', 'jk_nt1', 'jk_nt2', 'jk_t1', 'jk_t2', 'jk_ny', 'jk_ny1',
+    df.columns = ['course', 'humidity', 'kind', 'idx', 'name', 'cntry', 'gender', 'age', 'budam', 'jockey', 'trainer', 'owner',
+                  'weight', 'dweight', 'cnt', 'rcno', 'month', 'hr_days', 'hr_nt', 'hr_nt1', 'hr_nt2', 'hr_t1', 'hr_t2', 'hr_ny', 'hr_ny1',
+                  'hr_ny2', 'hr_y1', 'hr_y2', 'hr_dt', 'hr_d1', 'hr_d2', 'hr_rh', 'hr_rm', 'hr_rl',
+                  'jk_nt', 'jk_nt1', 'jk_nt2', 'jk_t1', 'jk_t2', 'jk_ny', 'jk_ny1',
                   'jk_ny2', 'jk_y1', 'jk_y2', 'tr_nt', 'tr_nt1', 'tr_nt2', 'tr_t1', 'tr_t2', 'tr_ny', 'tr_ny1',
-                  'tr_ny2', 'tr_y1', 'tr_y2', 'rcno']
+                  'tr_ny2', 'tr_y1', 'tr_y2']
     return df
 
 
 if __name__ == '__main__':
     meet = 1
-    date = 201610
+    date = 20161106
     data = parse_xml_entry(meet, date)
     print data
