b088a78c86641cc86cbc22865a47fce2b4ba338d
jyyang <yangjy0113@gmail.com>
2016-11-06 10:59:07 +0900

    add download function distance record

--- script/get_txt.py
+++ script/get_txt.py
@@ -48,5 +48,36 @@ def download_txt(bd, ed, meet):
                 print '[%s] data downloading failed' % request
     print "job has completed"
 
+def download_dist_rec(bd, ed, meet):
+    data = [# seoul http://race.kra.co.kr/chulmainfo/chulmaDetailInfoDistanceRecord.do?Act=02&Sub=1&meet=1&rcNo=3&rcDate=20070415
+        #      http://race.kra.co.kr/chulmainfo/chulmaDetailInfoDistanceRecord.do?Act=02&Sub=1&meet=1&rcNo=5&rcDate=20070714
+            [["http://race.kra.co.kr/chulmainfo/chulmaDetailInfoDistanceRecord.do?Act=02&Sub=1", [5, 6]]],
+            [["http://race.kra.co.kr/chulmainfo/chulmaDetailInfoDistanceRecord.do?Act=02&Sub=1", [4, 5]]],
+            [["http://race.kra.co.kr/chulmainfo/chulmaDetailInfoDistanceRecord.do?Act=02&Sub=1", [4, 6]]],
+    ]
+    for line in data[meet-1]:
+        race_url = line[0]
+        date = bd + datetime.timedelta(days=-1)
+        while date < ed:
+            date += datetime.timedelta(days=1)
+            if date.weekday() not in line[1]:
+                continue
+            for rcno in range(15, 20):
+                date_s = int("%d%02d%02d" % (date.year, date.month, date.day))
+                request = "%s&meet=%d&rcNo=%d&rcDate=%d" % (race_url, meet, rcno, date_s)
+                try:
+                    fname = "../txt/%d/dist_rec/dist_rec_%d_%d_%d.txt" % (meet, meet, date_s, rcno)
+                    if os.path.exists(fname):
+                        continue
+                    response_body = urlopen(request).read()
+                    fout = open(fname, 'w')
+                    fout.write(response_body)
+                    fout.close()
+                    print "[%s] data is downloaded" % request
+                except:
+                    print '[%s] data downloading failed' % request
+    print "job has completed"
+
+
 if __name__ == '__main__':
-    download_txt(datetime.date(2011, 1, 1), datetime.date.today(), 3)
+    download_dist_rec(datetime.date(2014, 7, 27), datetime.date(2014, 7, 27), 1)
