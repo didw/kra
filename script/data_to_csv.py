a23c6ce61a8bc681cf9509ed4ed33f924ba10851
jyyang <yangjy0113@gmail.com>
2016-11-08 20:02:44 +0900

    add data_to_csv

---
+++ script/data_to_csv.py
@@ -0,0 +1,37 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+import parse_txt_race as pr
+import datetime
+import pandas as pd
+import os.path
+
+
+def get_data(begin_date, end_date, fname_csv):
+    train_bd = begin_date
+    train_ed = end_date
+    date = train_bd
+    data = pd.DataFrame()
+    first = True
+    date += datetime.timedelta(days=-1)
+    while date < train_ed:
+        date += datetime.timedelta(days=1)
+        if date.weekday() != 5 and date.weekday() != 6:
+            continue
+        filename = "../txt/2/rcresult/rcresult_2_%02d%02d%02d.txt" % (date.year, date.month, date.day)
+        if not os.path.isfile(filename):
+            continue
+        if first:
+            data = pr.get_data_w_date(filename)
+            first = False
+        else:
+            data = data.append(pr.get_data_w_date(filename), ignore_index=True)
+    data.to_csv(fname_csv, index=False)
+    return data
+
+
+if __name__ == '__main__':
+    begin_date = datetime.date(2007, 1, 1)
+    end_date = datetime.date.today()
+    fname_csv = '../data/2_2007_2016.csv'
+    get_data(begin_date, end_date, fname_csv)
