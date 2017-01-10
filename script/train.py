9d0154643e25c6951a154dafecae47954302d4e6
Jongyeol Yang <yangjy0113@gmail.com>
2017-01-10 22:37:46 +0900

    fix to train date

--- script/train.py
+++ script/train.py
@@ -376,7 +376,7 @@ def simulation_weekly_train0(begin_date, end_date, delta_day=0, delta_year=0, co
                     score = estimator.score(X_test, Y_test)
                     print("Score with the entire test dataset = %.5f" % score)
                     pred = estimator.predict(X_test)
-                    
+                    """
                     res1 = sim.simulation7(pred, R_test, [[1],[2],[3]])
                     res2 = sim.simulation7(pred, R_test, [[1,2],[1,2,3],[1,2,3]])
                     res3 = sim.simulation7(pred, R_test, [[1,2,3],[1,2,3,4,5],[1,2,3,4,5,6]])
@@ -397,7 +397,7 @@ def simulation_weekly_train0(begin_date, end_date, delta_day=0, delta_year=0, co
                                                         ])
                     res6 = sim.simulation6(pred, R_test, [[2,3,4], [2,3,5], [2,4,5], [3,4,5], [2,3,6], [2,4,6], [2,5,6], [3,4,6], [3,5,6], [4,5,6]])
                     res7 = sim.simulation6(pred, R_test, [[3,4,5], [3,4,6], [3,4,7], [3,5,6], [3,5,7], [3,6,7], [4,5,6], [4,5,7], [4,6,7], [5,6,7]])
-                    
+                    """
                     res1 = sim.simulation2(pred, R_test, 1)
                     res2 = sim.simulation2(pred, R_test, 2)
                     res3 = sim.simulation2(pred, R_test, 3)
