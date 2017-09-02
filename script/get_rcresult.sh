cc5bd5036dd88438291dc1e5b34d01ef6e1899f8
Jongyeol Yang <yangjy0113@gmail.com>
2017-09-02 07:33:43 +0000

    minor change

--- script/get_rcresult.sh
+++ script/get_rcresult.sh
@@ -1,3 +1,5 @@
 wget https://www.dropbox.com/s/aoxxp0htbw35cpc/rcresult.zip?dl=0
-mv rcresult.zip
-unzip rcresult.zip ../txt/1/
+mv rcresult.zip\?dl\=0 rcresult.zip
+unzip rcresult.zip
+mkdir -p ../txt/1
+mv rcresult ../txt/1/
